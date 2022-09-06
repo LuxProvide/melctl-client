571a2abff03c57d2b11bf5ccd66df6dbfc589887
jpclipffel <jean-philippe.clipffel@lxp.lu>
2022-09-06 15:57:45 +0200

    [Style] Remove extra spaces

--- melctl_client/endpoints/ping.py
+++ melctl_client/endpoints/ping.py
@@ -9,4 +9,3 @@ class Ping(SimpleEndpoint):
         super().__init__(subparser, 'ping', 'GET', 'ping?seconds={seconds}', wait=False)
         self.parser.add_argument('-s', '--seconds', dest='seconds', type=int,
             default=0, help='Ping time')
-        
