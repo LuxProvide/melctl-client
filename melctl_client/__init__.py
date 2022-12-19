28099f4e91e94e3d966d6fb323403bd3519915ca
jpclipffel <jean-philippe.clipffel@lxp.lu>
2022-12-19 18:19:35 +0100

    [Feat] Numped version

--- melctl_client/__init__.py
+++ melctl_client/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '4.1.7'
-__version_name__ = 'Specific Virmac'
+__version__ = '5.0.0'
+__version_name__ = 'Pedantic Santa'
 __author__ = 'Jean-Philippe Clipffel <jean-philippe.clipffel@lxp.lu>'
